fd94320373a39b32ef4185fba5dc3dc648c2a0e2
Jose Carbonell <carbonellperez7@gmail.com>
2017-02-02 18:28:51 -0400

    Update returnJSON.cs

--- returnJSON.cs
+++ returnJSON.cs
@@ -3,8 +3,9 @@
         public ActionResult ViewName(<Parameters>)
         {
             var client = new JsonServiceClient("<Url WebApi>");
-            var Items = client.Get<Dto>("To complete url request");     
-            var model = Items.Where(x => x.cod.Trim() == line).Select(x => x.art).Distinct().ToList();
+            var List_items = client.Get<Dto>("To complete url request"); 
+            // Use LINQ(Lambda).
+            var model = List_items.Where(x => x.cod.Trim() == line).Select(x => x.art).Distinct().ToList();
             List<string> jsonList = h5;
             return Json(jsonList, JsonRequestBehavior.AllowGet);
             
